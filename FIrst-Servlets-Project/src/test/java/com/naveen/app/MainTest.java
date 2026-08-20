93020fd5191b9e0a0e0ac68fbdc5a4fd33964afa
naveenrasanka <naveenrasanka@gmail.com>
2026-08-21 04:02:59 +0530

    first servlet

---
+++ FIrst-Servlets-Project/src/test/java/com/naveen/app/MainTest.java
@@ -0,0 +1,13 @@
+package com.naveen.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void test(){
+        System.out.println("Test");
+    }
+}
