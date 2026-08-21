74e81ceb728bfa80eed7ef56e83c2a25ca567867
naveenrasanka <naveenrasanka@gmail.com>
2026-08-21 22:09:09 +0530

    servlet project 2

---
+++ Second-Servlet-Project/src/main/java/com/naveen/app/secondservletproject/ServletLifeCycle.java
@@ -0,0 +1,36 @@
+package com.naveen.app.secondservletproject;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/ServletLife")
+public class ServletLifeCycle extends HttpServlet {
+    static {
+        System.out.println("static Servlet is loaded....");
+    }
+    public ServletLifeCycle(){
+        System.out.println("Servlet obj is created!!");
+    }
+     public void init(ServletConfig config)throws ServletException{
+
+     }
+    public void service(HttpServletRequest request,
+                        HttpServletResponse response)throws ServletException, IOException {
+        System.out.println("Handle Request Response back");
+
+//           PrintWriter writer= response.getWriter();
+//           writer.println("hellow");
+
+    }
+    public void destroy(){
+
+    }
+
+}
---
+++ Second-Servlet-Project/src/main/java/com/naveen/app/secondservletproject/indexMain.java
@@ -0,0 +1,32 @@
+package com.naveen.app.secondservletproject;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/firstServlet")
+public class indexMain extends HttpServlet {
+    public void FirstServlet(){
+        System.out.println("servlet obj is created internally");
+    }
+
+    public void doPost(HttpServletRequest request,
+                          HttpServletResponse response)throws ServletException,IOException
+    {
+       String name= request.getParameter("name");
+       String city=request.getParameter("City");
+
+       PrintWriter writers= response.getWriter();
+       writers.println("<html><head><title>SecondApp</title></head>");
+       writers.println("<body bgcolor='cyan'><h1>Welcome to our Dynamic App</h1>");
+       writers.println("<h1>Hello "+name);
+       writers.println("I know you are from "+city);
+       writers.println("</h1></body></html>");
+       writers.close();
+    }
+}
