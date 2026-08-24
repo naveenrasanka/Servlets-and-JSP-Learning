8d13e137e19ec9e341a4d864fb4d2d6556ee4785
naveenrasanka <naveenrasanka@gmail.com>
2026-08-25 01:25:37 +0530

    learned basic about JSP

--- Third-Servlet-Project/src/main/java/com/naveen/app/thirdservletproject/RegisterFromServlet.java
+++ Third-Servlet-Project/src/main/java/com/naveen/app/thirdservletproject/RegisterFromServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -25,32 +26,45 @@ public class RegisterFromServlet extends HttpServlet {
         String Email= request.getParameter("email");
         String password= request.getParameter("password");
         String city= request.getParameter("city");
-
+           PrintWriter writer1= response.getWriter();
         String url="jdbc:mysql://localhost:3306/ServletLearn";
         String user="root";
         String pwd="1234";
         String query="INSERT INTO users(uname,uemail,upassword,ucity)VALUES(?,?,?,?)";
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //important
             Connection con = DriverManager.getConnection(url,user,pwd);
+            if(con !=null){
+
+                System.out.println("Connection is Successfull");
+            }
             PreparedStatement ps=con.prepareStatement(query);
             ps.setString(1,name);
             ps.setString(2,Email);
             ps.setString(3,password);
             ps.setString(4,city);
-            ps.executeUpdate();
+            int rowsAffected=ps.executeUpdate();
 
-            if(con !=null){
-                System.out.println("Connection is Successfull");
+            PrintWriter writer= response.getWriter();
+            if(rowsAffected!=0){
+                writer.println("<h1>Data Insert Successfully</h1>");
             }
-
-
+            ps.close();
+            writer.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        }catch (ClassNotFoundException m){
+
+            System.out.println(m.getMessage());
         }
 
-        PrintWriter writer= response.getWriter();
-           writer.println("<h1>Test</h1>");
 
+        //learning JSP part
+
+        HttpSession session= request.getSession();
+        session.setAttribute("name",name);
+        session.setAttribute("city",city);
+        session.setMaxInactiveInterval(10000);
 
     }
 }
---
+++ Third-Servlet-Project/src/main/webapp/Application.jsp
@@ -0,0 +1,32 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>JSP Web Application </title>
+</head>
+<body>
+<h1>JSP Web App to generate Dynamic</h1>
+<%@ page import="java.util.Date"
+
+%>
+<%!
+
+    int age=18;
+
+%>
+<%
+    Date date= new Date();
+
+    String name= request.getParameter("name");
+    String city= request.getParameter("city");
+
+    out.println(date);
+    out.println("Hello "+name);
+
+%>
+
+<h1><%= date %></h1>
+<h2><%= name %></h2>
+<h2><%= city %></h2>
+</body>
+</html>
